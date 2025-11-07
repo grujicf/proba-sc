a8932b5ec4d05cca20e85f6669491996ec86897c
grujicf <61869071+grujicf@users.noreply.github.com>
2025-11-07 14:38:22 +0100

    Update RandomContract3.sol

--- meow/random/RandomContract3.sol
+++ meow/random/RandomContract3.sol
@@ -3,10 +3,18 @@ pragma solidity 0.8.19;
 contract RandomContract3 {
     uint public number = 20;
     string public name = "filip";
+    address[] storage public adrese;
 
     constructor() {
         number = 20;
         name = "filip";
+    }
+
+    function updateAddr(uint _number, address[] calldata _adrese) public {
+        number = _number;
+        for(uint256 i = 0; i < _adrese.length; i++) {
+            adrese[i] = _adrese[i];
+        }
     }
 
     function update(uint _number, string memory _name) public {
